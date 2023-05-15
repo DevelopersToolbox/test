dc0dbefcaf5c688ad6c516903f64ca6c1064a947
Wolf <wolf@tgwolf.com>
2023-05-15 11:58:57 +0100

    Reusable pydocstyle

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 """
- This is a simple test script
+This is a simple test script.
 
-  It contains a multiline comment
+It contains a multiline comment
 """
 
 print("Hello, World!")
